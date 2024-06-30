71ceb3f62fe10f0b82d366591c354b664c7e69b7
huydn04 <luckyguy52431@gmail.com>
2024-06-30 22:35:28 +0700

    chore: config router

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,7 +3,6 @@ import { Outlet, Link } from "react-router-dom";
 const MainLayout = () => {
   return (
     <div>
-    
       <Outlet />
     </div>
   );
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,30 +1,29 @@
-import { Suspense, lazy } from "react";
+import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
-import ErrorBoundary from "../components/ErrorBoundary";
-import Pages404 from "../pages/404/Page404";
-
 
 const LandingPage = lazy(() => import("../pages/LandingPage"));
 const AdminPages = lazy(() => import("../pages/admin/AdminPages"));
+const Pages404 = lazy(() => import("../pages/404/Page404"));
 
 export default function AppRoutes() {
   return (
     <div className="">
-          <Routes>
-            <Route index path="/" element={<LandingPage />} />
+      <Routes>
+        {/* Landing Page  */}
+        <Route index path="/" element={<LandingPage />} />
 
-            {/* Amin Page  */}
-            <Route element={<MainLayout />}>
-              <Route path="/admin" element={<AdminPages />} />
-            </Route>
+        {/* Amin Page  */}
+        <Route element={<MainLayout />}>
+          <Route path="/admin" element={<AdminPages />} />
+        </Route>
 
-            {/* Page 404 */}
-            <Route element={<MainLayout />}>
-              <Route errorElement={<Pages404 />} />
-              <Route path="*" element={<Pages404 />} />
-            </Route>
-          </Routes>
+        {/* Page 404 */}
+        <Route element={<MainLayout />}>
+          <Route errorElement={<Pages404 />} />
+          <Route path="*" element={<Pages404 />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
