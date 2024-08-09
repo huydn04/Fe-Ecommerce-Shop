2d91546a9b43b07376af75e05d133fb8c47555ab
P1t-Web-Dev <trunghieuacc2@gmail.com>
2024-08-09 14:35:48 +0700

    feat: Modal-Update

--- src/components/admin/showproduct/Productmanager-button.jsx
+++ src/components/admin/showproduct/Productmanager-button.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { FaRegEdit } from "react-icons/fa";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import ModalUpdate from '../../modal/ModalUpdate';
+import { useState } from 'react';
 
 const Popup = () => {
+
+    const [showUpdate, setShowUpdate] = useState(false)
+
     return(
         <div className='h-[50px] rounded-md flex justify-around absolute ml-8 bottom-0 '>
-                <button className='w-[100px]  bg-[#d0cccd] text-xl flex justify-center rounded-md items-center hover:bg-[#fbb827] mr-2'><FaRegEdit className='mr-3' />Sửa</button>
+                <button onClick={() => setShowUpdate(true)} className='w-[100px]  bg-[#d0cccd] text-xl flex justify-center rounded-md items-center hover:bg-[#fbb827] mr-2'><FaRegEdit className='mr-3' />Sửa</button>
                 <button className='w-[100px]  bg-[#d0cccd] text-xl flex justify-center rounded-md items-center hover:bg-[#f74a5d]'><RiDeleteBin6Line className='mr-3'/> Xoá</button>
+
+                
+                <ModalUpdate isVisible={showUpdate} onClose={() => setShowUpdate(false)}/>
+                
         </div>
     )
 }
